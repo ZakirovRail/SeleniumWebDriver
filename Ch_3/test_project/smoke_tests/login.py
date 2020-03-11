28040b6d87606814009be9f6d6737f936c9d1086
R F <R@gmail.com>
2020-03-11 16:47:36 +0100

    Chapter 3_4 - finished

---
+++ Ch_3/test_project/smoke_tests/login.py
@@ -0,0 +1,7 @@
+# номер 3
+def test_guest_can_login(browser, language):
+    print("Passed номер 3")
+# номер 4
+class TestLogin(object):
+    def test_guest_should_see_login_link(self, browser, language):
+        print("Passed номер 4")
---
+++ Ch_3/test_project/test_regression.py
@@ -0,0 +1,19 @@
+class TestLessonCreate():
+    # номер 5
+    def test_create_lesson(self):
+        print("Passed номер 5")
+
+    # номер 6
+    def user_with_lesson_can_create_lesson_from_navbar_test(self):
+        print("Passed номер 6")
+
+
+class CourseCreate():
+    # номер 7
+    def test_create_course(self):
+        print("Passed номер 7")
+
+
+# номер 8
+def test_guest_can_open_new_course():
+    print("Passed номер 8")
