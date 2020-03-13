2bd3e4007e2d9217db2d425ea3284753d32a7324
R F <R@gmail.com>
2020-03-13 16:09:03 +0100

    Chapter 3_6 - final tasks finished

--- Ch_3/task_3_6_final/test_items.py
+++ Ch_3/task_3_6_final/test_items.py
@@ -5,5 +5,6 @@ link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_check_basket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn-add-to-basket")
-    time.sleep(30)
+    button_here = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button_here, "Button not found"
+    time.sleep(7)
