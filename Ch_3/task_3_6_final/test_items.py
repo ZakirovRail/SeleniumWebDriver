ce1dfeca9251575452942de381bc7c8ceee98661
R F <R@gmail.com>
2020-03-13 10:11:04 +0100

    Chapter 3_6 - final tasks finished

---
+++ Ch_3/task_3_6_final/test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_check_basket_button(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("button.btn-add-to-basket")
+    time.sleep(30)
